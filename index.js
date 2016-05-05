8da0400b519b10edc182c8a0146651c2dafe2790
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2016-05-04 23:33:15 -0700

    adding Layout rendering options

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
-'use strict';
+"use strict";
 
-const Hoek = require('hoek');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
+const Hoek = require("hoek");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
 
-
-const EXT_REGEX = new RegExp('\\.jsx$');
+const PLACEHOLDER = "%%PLACEHOLDER%%";
+const EXT_REGEX = new RegExp("\\.jsx$");
 const DEFAULTS = {
-    doctype: '<!DOCTYPE html>',
-    renderMethod: 'renderToStaticMarkup',
-    removeCache: process.env.NODE_ENV !== 'production'
+	doctype: "<!DOCTYPE html>",
+	renderMethod: "renderToStaticMarkup",
+	removeCache: process.env.NODE_ENV !== "production",
+	layout: "html.js",
+	layoutPath: `${__dirname}/components/`,
+	layoutKeyword: "children",
+	layoutRenderMethod: "renderToStaticMarkup"
 };
 
 
 const compile = function compile(template, compileOpts) {
+	compileOpts = Hoek.applyToDefaults(DEFAULTS, compileOpts);
 
-    compileOpts = Hoek.applyToDefaults(DEFAULTS, compileOpts);
+	return function runtime(context, renderOpts) {
+		renderOpts = Hoek.applyToDefaults(compileOpts, renderOpts);
 
-    return function runtime(context, renderOpts) {
+		let Component = require(compileOpts.filename);
+		// support es6 default export semantics
+		Component = Component.default || Component;
 
-        renderOpts = Hoek.applyToDefaults(compileOpts, renderOpts);
+		let element = React.createFactory(Component);
 
-        let Component = require(compileOpts.filename);
-        // support es6 default export semantics
-        Component = Component.default || Component;
+		let output = renderOpts.doctype;
 
-        let Element = React.createFactory(Component);
+		if (renderOpts.layout) {
+			let Layout = require(renderOpts.layoutPath + renderOpts.layout);
+			Layout = Layout.default || Layout;
+			let layoutElement = React.createFactory(Layout);
+			let layoutContext = Hoek.applyToDefaults({}, context);
+			layoutContext[renderOpts.layoutKeyword] = PLACEHOLDER;
 
-        let output = renderOpts.doctype;
-        output += ReactDOMServer[renderOpts.renderMethod](Element(context));
+			let layoutOutput = ReactDOMServer[renderOpts.layoutRenderMethod](layoutElement(layoutContext));
+			let elementOutput = ReactDOMServer[renderOpts.renderMethod](element(context));
+			output = layoutOutput.replace(PLACEHOLDER, elementOutput);
+		} else {
+			output += ReactDOMServer[renderOpts.renderMethod](element(context));
+		}
 
-        // transpilers tend to take a long time to start up, so we delete react
-        // modules from the cache so we don't need to restart to see view
-        // changes (unless we're in production silly)
-        if (renderOpts.removeCache) {
-            Component = undefined;
-            Element = undefined;
+		// transpilers tend to take a long time to start up, so we delete react
+		// modules from the cache so we don't need to restart to see view
+		// changes (unless we're in production silly)
+		if (renderOpts.removeCache) {
+			Component = undefined;
+			element = undefined;
 
-            Object.keys(require.cache).forEach((module) => {
+			Object.keys(require.cache).forEach((module) => {
+				if (EXT_REGEX.test(module)) {
+					delete require.cache[module];
+				}
+			});
+		}
 
-                if (EXT_REGEX.test(module)) {
-                    delete require.cache[module];
-                }
-            });
-        }
-
-        return output;
-    };
+		return output;
+	};
 };
 
 
 module.exports = {
-    compile: compile
+	compile: compile
 };
