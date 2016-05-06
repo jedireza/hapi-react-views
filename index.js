dc89960f267e09f4d6297fbc0519eb9625dc7bb2
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2016-05-06 15:52:26 -0700

    fixing linting

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
-"use strict";
+'use strict';
 
-const Hoek = require("hoek");
-const React = require("react");
-const ReactDOMServer = require("react-dom/server");
+const Hoek = require('hoek');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
 
-const PLACEHOLDER = "%%PLACEHOLDER%%";
-const EXT_REGEX = new RegExp("\\.jsx$");
+const PLACEHOLDER = '%%PLACEHOLDER%%';
+const EXT_REGEX = new RegExp('\\.jsx$');
 const DEFAULTS = {
-	doctype: "<!DOCTYPE html>",
-	renderMethod: "renderToStaticMarkup",
-	removeCache: process.env.NODE_ENV !== "production",
-	layout: false,
-	layoutPath: "",
-	layoutKeyword: "children",
-	layoutRenderMethod: "renderToStaticMarkup"
+    doctype: '<!DOCTYPE html>',
+    renderMethod: 'renderToStaticMarkup',
+    removeCache: process.env.NODE_ENV !== 'production',
+    layout: false,
+    layoutPath: '',
+    layoutKeyword: 'children',
+    layoutRenderMethod: 'renderToStaticMarkup'
 };
 
 
 const compile = function compile(template, compileOpts) {
-	compileOpts = Hoek.applyToDefaults(DEFAULTS, compileOpts);
 
-	return function runtime(context, renderOpts) {
-		renderOpts = Hoek.applyToDefaults(compileOpts, renderOpts);
+    compileOpts = Hoek.applyToDefaults(DEFAULTS, compileOpts);
 
-		let Component = require(compileOpts.filename);
-		// support es6 default export semantics
-		Component = Component.default || Component;
+    return function runtime(context, renderOpts) {
 
-		let element = React.createFactory(Component);
+        renderOpts = Hoek.applyToDefaults(compileOpts, renderOpts);
 
-		let output = renderOpts.doctype;
+        let Component = require(compileOpts.filename);
+        // support es6 default export semantics
+        Component = Component.default || Component;
 
-		if (renderOpts.layout) {
-			let Layout = require(renderOpts.layoutPath + renderOpts.layout);
-			Layout = Layout.default || Layout;
+        let element = React.createFactory(Component);
 
-			let layoutElement = React.createFactory(Layout);
-			let layoutContext = Hoek.applyToDefaults({}, context);
-			layoutContext[renderOpts.layoutKeyword] = PLACEHOLDER;
+        let output = renderOpts.doctype;
 
-			let layoutOutput = ReactDOMServer[renderOpts.layoutRenderMethod](layoutElement(layoutContext));
-			let elementOutput = ReactDOMServer[renderOpts.renderMethod](element(context));
-			output = layoutOutput.replace(PLACEHOLDER, elementOutput);
-		} else {
-			output += ReactDOMServer[renderOpts.renderMethod](element(context));
-		}
+        if (renderOpts.layout) {
+            let Layout = require(renderOpts.layoutPath + renderOpts.layout);
+            Layout = Layout.default || Layout;
 
-		// transpilers tend to take a long time to start up, so we delete react
-		// modules from the cache so we don't need to restart to see view
-		// changes (unless we're in production silly)
-		if (renderOpts.removeCache) {
-			Component = undefined;
-			element = undefined;
+            const layoutElement = React.createFactory(Layout);
+            const layoutContext = Hoek.applyToDefaults({}, context);
+            layoutContext[renderOpts.layoutKeyword] = PLACEHOLDER;
 
-			Object.keys(require.cache).forEach((module) => {
-				if (EXT_REGEX.test(module)) {
-					delete require.cache[module];
-				}
-			});
-		}
+            const layoutOutput = ReactDOMServer[renderOpts.layoutRenderMethod](layoutElement(layoutContext));
+            const elementOutput = ReactDOMServer[renderOpts.renderMethod](element(context));
+            output = layoutOutput.replace(PLACEHOLDER, elementOutput);
+        } else {
+            output += ReactDOMServer[renderOpts.renderMethod](element(context));
+        }
 
-		return output;
-	};
+        // transpilers tend to take a long time to start up, so we delete react
+        // modules from the cache so we don't need to restart to see view
+        // changes (unless we're in production silly)
+        if (renderOpts.removeCache) {
+            Component = undefined;
+            element = undefined;
+
+            Object.keys(require.cache).forEach((module) => {
+                if (EXT_REGEX.test(module)) {
+                    delete require.cache[module];
+                }
+            });
+        }
+
+        return output;
+    };
 };
 
 
 module.exports = {
-	compile: compile
+    compile: compile
 };
